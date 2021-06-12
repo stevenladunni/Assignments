9017a2bbfaa38e55456d0a1f72ff0f0b68da8108
steven <bladunni@gmail.com>
2021-06-12 01:40:51 -0500

    adding screenshot and naming it assignment 1 then adding comments to assignment 5

--- assignment_5.py
+++ assignment_5.py
@@ -1,8 +1,9 @@
 # To run this script type python assignment_5.py
+# Here we are reusing the variables "names_list" and "current_longest_names" from the last assignment but this time, we are defining a function that can be reused over and over for many different arguements or lists.
 names_list = ["bob", "jimmy", "max b", "bernie", "jordan","future hendrix"]
 def big_name(names):
     current_longest_name = ""
-    for name in names_list:
+    for name in names:
         if len(name)>len(current_longest_name):
             current_longest_name = name
         else:
